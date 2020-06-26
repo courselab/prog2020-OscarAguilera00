6fc25d514a8f54f3944265e51a4ff8f2052e0ab1
OscarAguilera00 <62971083+OscarAguilera00@users.noreply.github.com>
2020-06-26 15:53:38 -0600

    M003 correccao

--- m003.c
+++ m003.c
@@ -1,12 +1,12 @@
 /* m003.c - Check if a positive integer is prime.
 
-   This program takes a non-zero positive integer read as a 
+   This program takes a non-zero positive integer read as a
    command line argument and outputs 1 or 0 depending on
    whether the number is prime or not.
 
    E.g.
 
-      command line    : m003 5 
+      command line    : m003 5
       expected output : 1
 
    Directions:
@@ -21,8 +21,28 @@
 
 /* Verify if the positive integer 'a' is prime. */
 
-int is_prime (int a)
+int is_prime (int x)
 {
+     int p;
+
+  int count = 0;
+
+  for(p =1; p < x; p++)
+  {
+    if(( x %p) ==0)
+    {
+      count ++;
+    }
+  }
+  if(count == 1)
+  {
+    x = 1;
+  }
+  else
+  {
+    x = 0;
+  }
+  return x;
   return 0;
 }
 
@@ -40,10 +60,10 @@ int main (int argc, char **argv)
       exit(1);
     }
 
-  
+
   n = atoi (argv[1]);
 
   printf("%d\n", is_prime (n));
-  
+
   return 0;
 }
